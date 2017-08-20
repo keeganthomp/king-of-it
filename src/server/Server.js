678b7854d9c57498f3f4b50f3b90910b16ae3774
keeganthomp <whereiskeegan@gmail.com>
2017-08-20 09:35:14 -0500

    breaking up routes into individual files

--- src/server/Server.js
+++ src/server/Server.js
@@ -2,20 +2,11 @@ const express = require("express");
 const server = express();
 const axios = require('axios');
 const cors = require('cors')
+const apiRoutes = require('./apiRoutes')
 
 server.use(cors());
 
-server.get("/api/images", (req, res) => {
-  axios
-    .get(
-      `https://139167162543316:MPfnC3smU0G-vHs-GC2YBZHz0Ww@api.cloudinary.com/v1_1/keezee/resources/image`
-    )
-    .then(response => {
-      console.log("response ", response.data);
-      res.send(response.data);
-    })
-    .catch(err => console.log(err));
-});
+server.use('/api', apiRoutes)
 
 server.listen(7000, () => {
   console.log("running on port 7000");
---
+++ src/server/apiRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cloudinaryApi = express.Router();
+const axios = require('axios');
+const cors = require('cors')
+
+cloudinaryApi.get("/images", (req, res) => {
+  axios
+    .get(
+      `https://139167162543316:MPfnC3smU0G-vHs-GC2YBZHz0Ww@api.cloudinary.com/v1_1/keezee/resources/image`
+    )
+    .then(response => {
+      console.log("response ", response.data);
+      res.send(response.data);
+    })
+    .catch(err => console.log(err));
+});
+
+module.exports = api;
