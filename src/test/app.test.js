767191df9c4fa606de1d4b4c4e385e9c0b3cd90a
keeganthomp <whereiskeegan@gmail.com>
2017-08-22 12:54:59 -0500

    adding test to ensure each component is rendering properly

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,14 +1,34 @@
 import { expect } from "chai";
 import React from "react";
 import LoginPage from "../components/LoginPage";
+import SignupPage from "../components/SignupPage";
 import ImageDisplay from "../components/ImageDisplay";
+import ImageUploader from "../components/ImageUploader";
 import { mount, shallow } from "enzyme";
-import {store} from "../store";
+import { store } from "../store";
 import { Provider } from "react-redux";
 
 describe("<LoginPage/>", () => {
   it("Login Page Renders", () => {
     const wrapper = shallow(<LoginPage />);
+    expect(wrapper).to.have.length(1);
+  });
+});
+
+describe("<SignupPage/>", () => {
+  it("SignupPage Page Renders", () => {
+    const wrapper = shallow(<SignupPage />);
+    expect(wrapper).to.have.length(1);
+  });
+});
+
+describe("<ImageUploader/>", () => {
+  it("ImageUploader component renders", () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <ImageUploader />
+      </Provider>
+    );
     expect(wrapper).to.have.length(1);
   });
 });
