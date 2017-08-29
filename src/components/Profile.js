485cc4bff430e14e3c02b98e2e2802f6aeb6a85c
keeganthomp <whereiskeegan@gmail.com>
2017-08-29 16:28:17 -0500

    adding a home button on profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import "../styles/App.css";
 
 
@@ -10,6 +11,7 @@ export default class Profile extends Component {
             <div>
                 <h1>Profile Page</h1>
                 <h2>HI {this.props.match.params.currentUser}</h2>
+                <Link to={`/`}><button>Home</button></Link>
             </div>
         );
     }
